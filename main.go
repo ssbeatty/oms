1d985fd3ec2cfb40ed7a7b758fe14b7d8988774b
WangGuoXi <wang918562230@gmail.com>
2020-08-19 15:40:50 +0800

    定时任务

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/toolbox"
 	_ "oms/routers"
+	"oms/tasks"
 )
 
 func main() {
 	// static files
 	beego.SetStaticPath("/static", "static")
 
+	// tasks
+	toolbox.StartTask()
+	defer toolbox.StopTask()
+
+	test1 := toolbox.NewTask("test1", "0 0 * * * *", tasks.Task1)
+	toolbox.AddTask("test1", test1)
+
 	// main func
 	beego.Run()
+
 }
---
+++ tasks/task.go
@@ -0,0 +1,10 @@
+package tasks
+
+import "oms/logger"
+
+func Task1() error {
+	logger.Logger.Println("======================Task Task1 start======================")
+
+	logger.Logger.Println("======================Task Task1 stop ======================")
+	return nil
+}
